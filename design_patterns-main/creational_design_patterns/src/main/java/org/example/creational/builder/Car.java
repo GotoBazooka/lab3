65585f1f3c9f8981069d1652a17aa1750866eaf7
GotoBazooka <148977824+GotoBazooka@users.noreply.github.com>
2024-11-04 13:53:51 +0200

    Add files via upload

---
+++ design_patterns-main/creational_design_patterns/src/main/java/org/example/creational/builder/Car.java
@@ -0,0 +1,27 @@
+package org.example.creational.builder;
+
+// TODO implement logic
+public class Car {
+    private String engine;
+    private String transmission;
+    private String interior;
+    private String color;
+    private boolean hasSunroof;
+    private boolean hasGPS;
+    private boolean hasSafetyPackage;
+
+    private Car(Builder builder) {
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public static class Builder {
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+}
